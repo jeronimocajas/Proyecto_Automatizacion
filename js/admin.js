61921be826ccf1ef7519cf4b436ca51f3667b12a
jeronimocajas <125157113+jeronimocajas@users.noreply.github.com>
2026-05-25 13:58:54 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-const API_BASE="http://localhost:8000/api";
+const API_BASE = "https://auxilioscecar.up.railway.app/api";
 let adminUsuario="",adminPassword="",todosEstudiantes=[];
 
 async function login(){
