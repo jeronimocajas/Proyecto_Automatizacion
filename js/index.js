33fd9584dd33f747cf2da706d7ab78db55eba552
jeronimocajas <125157113+jeronimocajas@users.noreply.github.com>
2026-05-25 13:59:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "https://auxilioscecar.up.railway.app/api";
 let estado = { token:null, estudianteId:null, correo:null, cedula:null, tipoAuxilioSeleccionado:null, tiposAuxilio:[], archivoPDF:null, cartaPDF:null, intentosUsados:0 };
 let timerInterval = null, timerSegundos = 600;
 
